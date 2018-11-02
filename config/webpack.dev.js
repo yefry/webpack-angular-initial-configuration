0de7fb3d8936d7536d882227aec5a1c2f7b6519f
yeferson.marin <Yeferson.Marin@e-hps.com>
2018-11-02 12:09:16 -0500

    base configuration files

---
+++ config/webpack.dev.js
@@ -0,0 +1,45 @@
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+
+module.exports = webpackMerge(commonConfig, {
+  devtool: 'cheap-module-eval-source-map',
+
+  output: {
+    path: helpers.root('dist'),
+    publicPath: '/',
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+
+  devServer: {
+    historyApiFallback: true,
+    stats: 'minimal'
+  },
+
+  // Automatically compile when files change.
+  watch: true,
+   
+  // Automatically reload the page when compilation is done.
+   devServer: {
+    inline: true
+  },
+
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
+    }]
+  }
+});
